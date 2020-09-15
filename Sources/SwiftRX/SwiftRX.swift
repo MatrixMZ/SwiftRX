32a364bbac28147fbccb748fcc5eb0bc12ec1f03
Mateusz Ziobrowski <matrix.ziobrowski@gmail.com>
2020-09-15 11:50:22 +0100

    Docs & Integration tests added

--- Sources/SwiftRX/SwiftRX.swift
+++ Sources/SwiftRX/SwiftRX.swift
@@ -1,19 +1,160 @@
 import Foundation
 
-struct SwiftRX {
-    var text = "Hello, World!"
-}
+/**
+    Should be implemented with every redux action.
+ 
+    # Implementation
+    Enum type objects are the best option to implement it because it is easy to define action payload.
+    ```
+    enum PostAction: Action {
+        case LoadPosts(_ request: String)
+        case RemovePost(index: Int)
+        case AddOne(post: String)
+    }
+    ```
+    But if you do not like it you can use Struct as well.
+    
+ */
+protocol Action {}
 
-protocol Action {
-
-}
-
+/**
+    Used to define State type Struct in redux pattern.
+ 
+    # Implementation
+    Main state definition will be different comparing to substate.
+    Main:
+    ```
+    struct AppState: State {
+        var posts: PostState = initialPostState
+    }
+    ```
+    Sub:
+    ```
+    struct PostState: State {
+        let posts: [String]
+    }
+    
+    let initialPostState: PostState = PostState(posts: [])
+    ```
+ */
 protocol State {}
 
+/**
+    Can be used to create reducer.
+    Takes Action and State and returns updated copy of state.
+ 
+    # Implementation
+ 
+    For main state:
+    ```
+    func AppReducer(action: Action, state: AppState) -> AppState {
+        return AppState(posts: ((action as? PostAction) != nil) ? PostReducer(action: action as! PostAction, state: state.posts) : state.posts)
+    }
+    ```
+ 
+    For sub state
+    ```
+    func PostReducer(action: PostAction, state: PostState) -> PostState {
+        switch action {
+            case .LoadPosts:
+                print("LoadPost Action Dispatched")
+                break
+            case .RemovePost:
+                print("RemovePost Action Dispatched")
+                break
+            case .AddOne(post: let post):
+                return PostState(posts: [] + state.posts + [post])
+        }
+        
+        return state
+    }
+    ```
+ 
+    
+ */
 typealias Reducer<S: State> = (Action,  S) -> S
 
+/**
+    Used to dispatch async actions.
+ 
+    Async actions can be dispatched from inside of the fuction after the function returned Action or nil by simply placing them in Dispatch Queue.
+ 
+    # Implementation
+    ```
+     struct PostsActionCreator {
+         static let getPosts: ActionCreator = { state, store in
+             AF.request("https://jsonplaceholder.typicode.com/posts").response { response in
+                 let response: [Post] = try! JSONDecoder().decode([Post].self, from: response.data!)
+                 store.dispatch(PostAction.LoadPosts(.success(response: response)))
+             }
+ 
+             return PostAction.LoadPosts(.loading)
+         }
+     }
+    ```
+    Alternative:
+    ```
+    let LoadPosts: ActionCreator<AppState> = { state, store in
+         AF.request("https://jsonplaceholder.typicode.com/posts").response { response in
+            let response: [Post] = try! JSONDecoder().decode([Post].self, from: response.data!)
+            store.dispatch(PostAction.LoadPosts(.success(response: response)))
+        }
+ 
+        return PostAction.LoadPosts(.loading)
+    }
+    ```
+ 
+    - Parameters:
+        - state: State
+        - store: Store
+ 
+    - Returns: Optional<Action>
+ 
+ */
 typealias ActionCreator<S: State> = (_ state: S, _ store: Store<S>) -> Action?
 
+
+/**
+    This final class defines Store that access will be available in the entire application.
+    
+    # Implementation
+    In your ```SceneDelegate.swift```
+    ```
+    class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+        var store: Store<AppState> = Store<AppState>(initialState: AppState(), reducer: AppReducer)
+ 
+        ...
+ 
+        func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+ 
+            // It has to be injected as EnvironmentObject to the MainView
+            let appView = AppView().environmentObject(store)
+
+            if let windowScene = scene as? UIWindowScene {
+                let window = UIWindow(windowScene: windowScene)
+                window.rootViewController = UIHostingController(rootView: appView)
+                self.window = window
+                window.makeKeyAndVisible()
+            }
+        }
+    ```
+    So you can use it in view like so:
+    ```
+    struct AppView: View {
+        @EnvironmentObject var store: Store
+ 
+        ...
+    }
+    
+    ```
+ 
+    # Action | ActionCreator dispatcher
+    To dispatch an action or action creator use:
+    ```
+        store.dispatch(LoadPosts)
+    ```
+    This will update the state in store and also automatically refresh every view that was implementing this feature.
+ */
 final class Store<S: State>: ObservableObject {
     @Published private(set) var state: S
     private let reducer: Reducer<S>
--- Tests/SwiftRXTests/SwiftRXTests.swift
+++ Tests/SwiftRXTests/SwiftRXTests.swift
@@ -2,14 +2,81 @@ import XCTest
 @testable import SwiftRX
 
 final class SwiftRXTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftRX().text, "Hello, World!")
+    var sut: Store<AppState>!
+    
+    override func setUp() {
+        super.setUp()
+        sut = Store<AppState>(initialState: AppState(), reducer: AppReducer)
     }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    
+    func testCreateStore() {
+        XCTAssertEqual(sut.state.posts.posts, AppState().posts.posts, "After init store conatins different items")
+    }
+    
+    func testDispatchAction() {
+        sut.dispatch(PostAction.AddOne(post: "New Post"))
+        XCTAssertEqual(sut.state.posts.posts, ["New Post"], "After dispatching action the store does not contain expected value")
+    }
+    
+    func testDispatchMultipleActions() {
+        sut.dispatch(PostAction.AddOne(post: "Post1"))
+        sut.dispatch(PostAction.AddOne(post: "Post2"))
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+        XCTAssertEqual(sut.state.posts.posts, ["Post1", "Post2"], "After dispatching multiple action the store does not contain expected value")
+    }
+    
+    func testActionCreator() {
+           sut.dispatch(LoadPosts)
+
+           XCTAssertEqual(sut.state.posts.posts, ["From Action Creator"], "Action Creator does not return Action")
+       }
+
+}
+
+
+struct PostState: State {
+    let posts: [String]
+}
+
+let initialPostState: PostState = PostState(posts: [])
+
+enum PostAction: Action {
+    case LoadPosts(_ request: String)
+    case RemovePost(index: Int)
+    case AddOne(post: String)
+}
+
+func PostReducer(action: PostAction, state: PostState) -> PostState {
+    switch action {
+        case .LoadPosts:
+            print("LoadPost Action Dispatched")
+            break
+        case .RemovePost:
+            print("RemovePost Action Dispatched")
+            break
+        case .AddOne(post: let post):
+            return PostState(posts: [] + state.posts + [post])
+    }
+    
+    return state
+}
+
+struct AppState: State {
+    var posts: PostState = initialPostState
+}
+
+func AppReducer(action: Action, state: AppState) -> AppState {
+    return AppState(posts: ((action as? PostAction) != nil) ? PostReducer(action: action as! PostAction, state: state.posts) : state.posts)
+}
+
+let store = Store<AppState>(initialState: AppState(), reducer: AppReducer)
+
+let LoadPosts: ActionCreator<AppState> = { state, store in
+    return PostAction.AddOne(post: "From Action Creator")
 }
